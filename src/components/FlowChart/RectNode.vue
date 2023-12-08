7c6aa2bbe0c1e59321e77a15a549870fba29c3b6
Yang907 <67835698+Yang907@users.noreply.github.com>
2023-12-08 16:13:56 +0800

    Update RectNode.vue
    修改部分功能

--- src/components/FlowChart/RectNode.vue
+++ src/components/FlowChart/RectNode.vue
@@ -2,33 +2,25 @@
   <div
     class="rect-container"
     :class="{ 'high-light': highLight }"
-    @dblclick="handleDoubleClick()"
+    :id="'flow_node' + id"
   >
-    <div v-if="isEdit" class="node_c">
-      <!-- <span v-if="!isEdit">{{ code || "节点code" }}</span> -->
-      <input class="input" v-model="code" placeholder="code值" />
+    <div class="node_t" :style="{ color: textColor1 }">
+      <span>{{ lobelTop || "节点名称" }}</span>
     </div>
-    <div v-if="isEdit" class="node_c">
-      <!-- <span v-if="!isEdit">{{ code || "节点code" }}</span> -->
-      <input class="input" v-model="type" placeholder="节点类型" />
-    </div>
-    <div class="node_t">
-      <span v-if="!isEdit">{{ lobelTop || "节点名称" }}</span>
-      <input class="input" v-else v-model="lobelTop" placeholder="节点名称" />
-    </div>
-    <div class="node_b" :style="{ 'background-color': typeColor[type] }">
-      <span v-if="!isEdit">{{ lobelBottom || "角色名称" }}</span>
-      <input
-        class="input"
-        v-else
-        v-model="lobelBottom"
-        placeholder="角色名称"
-      />
+    <div
+      class="node_b"
+      :style="{
+        'background-color': typeColor[type],
+        color: textColor2,
+      }"
+    >
+      <span>{{ lobelBottom || "角色名称" }}</span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { typeColor } from "./config.js";
 export default {
   name: "RectNode",
   inject: ["getNode"],
@@ -39,13 +31,11 @@ export default {
       highLight: false,
       code: null,
       isEdit: false,
-      preCode: null,
       type: 1, // 对应节点类型1，2，3
-      typeColor: {
-        1: "#9bcdff",
-        2: "#c0f2ff",
-        3: "#c9d8b4",
-      },
+      id: null,
+      typeColor,
+      textColor1: "#2c3e50",
+      textColor2: "#2c3e50",
     };
   },
   mounted() {
@@ -53,67 +43,66 @@ export default {
     this.lobelTop = node.store.data.label1;
     this.lobelBottom = node.store.data.label2;
     this.highLight = node.store.data.highLight;
-    this.code = node.store.data.id;
+    this.code = node.store.data.code;
     this.type = node.store.data.type;
-    this.preCode = this.code;
-    window.addEventListener("keydown", this.myFunction);
-  },
-
-  destroyed() {
-    window.removeEventListener("keydown", this.myFunction);
-  },
-
-  methods: {
-    handleDoubleClick() {
-      this.isEdit = true;
-      const node = this.getNode();
-      node.prop("isEdit", true);
-    },
-    myFunction(e) {
-      const node = this.getNode();
-      // const preId = node.id;
-
-      if (e.altKey && e.key.toLowerCase() == "s") {
-        this.isEdit = false;
-        node.prop("label1", this.lobelTop);
-        node.prop("label2", this.lobelBottom);
-        node.prop("id", this.code);
-        node.prop("isEdit", false);
-        node.prop("type", this.type);
-        // id被修改，需要添加标识，以便重新渲染node
-        if (this.preCode != this.code) {
-          node.prop("isUpdate", true);
-        }
-      }
-    },
+    this.id = node.id;
+    node.on("change:code", ({ current }) => {
+      this.code = current;
+    });
+    node.on("change:label1", ({ current }) => {
+      this.lobelTop = current;
+    });
+    node.on("change:label2", ({ current }) => {
+      this.lobelBottom = current;
+    });
+    node.on("change:type", ({ current }) => {
+      this.type = current;
+    });
+    node.on("change:textColor1", ({ current }) => {
+      this.textColor1 = current || "#2c3e50";
+    });
+    node.on("change:textColor2", ({ current }) => {
+      this.textColor2 = current || "#2c3e50";
+    });
   },
 };
 </script>
 <style scoped>
 .rect-container {
   border: 1px solid #5f95ff;
+  box-sizing: border-box;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 
-.node_c {
-  font-size: 12px;
-  border-bottom: 1px solid #000;
-  padding: 2px;
-}
 .node_t {
   font-size: 12px;
-  border-bottom: 1px solid #000;
-  padding: 2px;
+  box-sizing: border-box;
+  /* padding: 2px; */
+  height: 55%;
+  background: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .node_b {
   font-size: 12px;
-  padding: 2px;
+  border-top: 1px solid #000;
+  /* padding: 2px; */
   background-color: #9bcdff;
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .input {
   width: calc(100% - 8px);
 }
 .high-light {
-  border: 2px dashed red;
+  border: 2px dashed #03e9f4;
   background-color: rgb(124, 104, 252, 0.2);
+  box-shadow: 0 0 5px #03e9f4, 0 0 25px #03e9f4, 0 0 50px #03e9f4,
+    0 0 100px #03e9f4;
 }
 </style>
